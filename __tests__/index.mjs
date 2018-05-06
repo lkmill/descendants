2bd3a63bcf7d10489437348a12851842a67754a9
Linus Miller <lohfu@lohfu.io>
2018-05-06 19:50:08 +0200

    Set up basic tests~

---
+++ __tests__/index.mjs
@@ -0,0 +1,99 @@
+import find from '../src/index'
+
+const strs = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+]
+
+const html = `
+${strs[0]}
+
+<div id='a-1' class='a a-1'>
+  ${strs[1]}
+  
+  <div id='a-2' class='a a-2'>
+    ${strs[2]}
+
+    <div id='a-3' class='a a-3'>
+      ${strs[3]}
+
+      <div id='a-4' class='a a-4'>
+        ${strs[4]}
+      </div>
+    </div>
+  </div>
+</div>
+`
+  .trim()
+  .split(/>\s+</)
+  .join('><')
+  .split(/>\s+(?=\w)/)
+  .join('>')
+  .replace(/(\w)\s+</g, '$1<')
+
+document.body.innerHTML = html
+
+const a1 = document.getElementById('a-1')
+const a2 = document.getElementById('a-2')
+const a3 = document.getElementById('a-3')
+const a4 = document.getElementById('a-4')
+
+test('basic check', () => {
+  let result = find(a1, {
+    selector: '#a-1',
+  })
+
+  expect(result).toEqual([])
+
+  result = find(a1, {
+    nodeType: 1,
+  })
+
+  expect(result).toEqual([ a2, a3, a4 ])
+
+  result = find(a1, {
+    nodeType: 3,
+  }).map((node) => node.textContent)
+
+  expect(result).toEqual(strs.slice(1))
+
+  result = find(a1, {
+    nodeType: 1,
+    levels: 1,
+  })
+
+  expect(result).toEqual([ a2 ])
+
+  result = find(a1, {
+    selector: '.a-3',
+  })
+
+  expect(result).toEqual([ a3 ])
+
+  result = find(a1, {
+    selector: '.a',
+  })
+
+  expect(result).toEqual([ a2, a3, a4 ])
+
+  result = find(a1, {
+    selector: '#a-2',
+  })
+
+  expect(result).toEqual([ a2 ])
+
+  result = find(a1, {
+    filter: () => false,
+  })
+
+  expect(result.length).toBe(0)
+
+  result = find(a1, {
+    filter: () => true,
+  })
+
+  expect(result.length).toBe(7)
+})
